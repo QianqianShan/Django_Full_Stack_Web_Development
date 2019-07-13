94fe5884d519a4e750dd5305406a7e3c4712d703
Qianqian Shan <qianqianshan.am@gmail.com>
2019-07-13 19:27:35 -0400

    python manage.py migrate
    models.py
    pipenv isntall django-imagekit
    update INSTALLED_APPS (add imagekit) in settings.py
    create Post class in models.py under app
    
    Every time after changing models.py:
    python manage.py makemigrations --> compare models with current database, produce 0001_initial.py for changes
    
    python manage.py migrate --> apply the above changes
    
    Update Admin:
    background: Content Management System (CMS)
    
    python manage.py createsuperuser

--- FunApp/models.py
+++ FunApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
 
 # Create your models here.
+# create a post class inherited from models.Model
+# Post includes two fields: title and image
+class Post(models.Model):
+    # https://docs.djangoproject.com/en/2.2/ref/models/fields/
+    # can be blank / null
+    title = models.TextField(blank = True, null = True)
+    image = ProcessedImageField(
+    upload_to = 'static/images/posts',
+    format = 'JPEG',
+    options = {'quality':100},
+    blank = True,
+    null = True,
+    )
+
+    def __str__(self):
+        return self.title
