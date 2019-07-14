8dd7a8cc6224330b2f166ab2dc5d131716029f0e
Qianqian Shan <qianqianshan.am@gmail.com>
2019-07-14 19:04:21 -0400

    Add Comment and Like

--- FunApp/admin.py
+++ FunApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from FunApp.models import Post, FunAppUser
+from FunApp.models import Post, FunAppUser, Comment, Like
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(FunAppUser)
+admin.site.register(Comment)
+admin.site.register(Like)
--- FunApp/models.py
+++ FunApp/models.py
@@ -6,9 +6,25 @@ from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class FunAppUser(AbstractUser):
+    # customized profile picture
+    profile_pic = ProcessedImageField(
+                upload_to = 'static/images/profiles',
+                format = 'JPEG',
+                options = {'quality':200},
+                blank = True,
+                null = True,
+                )
+
 # create a post class inherited from models.Model
 # Post includes two fields: title and image
 class Post(models.Model):
+    author = models.ForeignKey(
+             FunAppUser,
+             on_delete = models.CASCADE,
+             related_name = 'posts',
+             blank = True,
+             null = True,)
     # https://docs.djangoproject.com/en/2.2/ref/models/fields/
     # title and image can be blank / null
     title = models.TextField(blank = True, null = True)
@@ -29,12 +45,39 @@ class Post(models.Model):
         # reverse url from its name
         return reverse('post_detail', args = [str(self.id)])
 
-class FunAppUser(AbstractUser):
-    # customized profile picture
-    profile_pic = ProcessedImageField(
-                upload_to = 'static/images/profiles',
-                format = 'JPEG',
-                options = {'quality':200},
-                blank = True,
-                null = True,
-                )
+
+
+class Comment(models.Model):
+    # foreignkey of database linked to Post
+    # related_name = 'comments' so that post.comments
+     # can show all comments of current Post
+    post = models.ForeignKey(to = Post,
+           on_delete = models.CASCADE,
+           related_name = 'comments')
+    # who wrote the comments
+    user = models.ForeignKey(
+           FunAppUser,
+           on_delete = models.CASCADE,
+           related_name = 'comments'
+    )
+
+    comment = models.CharField(max_length = 140)
+    posted_on = models.DateTimeField(
+                auto_now_add = True,
+                editable = False)
+
+    def __str__(self):
+        return self.comment
+
+class Like(models.Model):
+    post = models.ForeignKey(Post,
+           on_delete = models.CASCADE,
+           related_name = 'likes',)
+    user = models.ForeignKey(FunAppUser,
+           on_delete = models.CASCADE)
+
+    # class Meta:
+    #     unique_together = ("post", "user")
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' ' + self.post.title
